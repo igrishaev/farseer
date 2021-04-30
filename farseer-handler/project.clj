7fb4e8731f67821c2cb2d7d497b6ca3f088a47cf
Ivan Grishaev <ivan@grishaev.me>
2021-04-30 17:30:46 +0300

    sub handler

--- farseer-handler/project.clj
+++ farseer-handler/project.clj
@@ -1,16 +1,27 @@
 (defproject farseer-handler "0.1.0-SNAPSHOT"
 
+  :description
+  "The basic, transport-agnostic RPC handler"
+
   :plugins [[lein-parent "0.3.8"]]
 
   :parent-project
   {:path "../project.clj"
-   :inherit [:repositories :scm :deploy-repositories
-             :managed-dependencies :description :url :license
-             :plugins :test-selectors [:profiles :test :plugins]]}
+   :inherit [:deploy-repositories
+             :license
+             :managed-dependencies
+             :plugins
+             :repositories
+             :scm
+             :test-selectors
+             :url
+             [:profiles :dev]
+             [:profiles :test :plugins]]}
 
   :profiles
   {:dev
-   {:dependencies
+   {:resource-paths ["../dev-resources"]
+    :dependencies
     [[ch.qos.logback/logback-classic]]}}
 
   :dependencies
